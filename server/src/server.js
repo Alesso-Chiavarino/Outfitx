5a43943cf72915921e3dc7c89785202c10ac2b81
alesso-chiavarino <alessochiavarino@gmail.com>
2023-06-27 19:11:02 -0300

    go to desktop

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,11 @@ import apiRouter from './routers/app.routes.js'
 import { addLogger } from './middlewares/logger.middleware.js'
 import { serve as swaggerServe, setup as swaggerSetup } from 'swagger-ui-express'
 import { specs } from './config/swagger.config.js'
+import handlebars from 'express-handlebars'
+import path from 'path'
+import __dirname from './utils/dirname.utils.js'
+
+
 const app = express()
 
 const { PORT } = ENV
@@ -19,6 +24,11 @@ app.use(cors({
 app.use(addLogger)
 app.use('/api', apiRouter)
 app.use('/api/doc', swaggerServe, swaggerSetup(specs))
+
+//views
+app.engine('handlebars', handlebars.engine())
+app.set('views', path.resolve(__dirname, './views'));
+app.set('view engine', 'handlebars');
 
 const server_url = `http://localhost:${PORT}`
 const server = app.listen(PORT, () => logSuccess(`server is running in ${server_url}`)) 
